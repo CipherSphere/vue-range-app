6708a69a1e2ae5cf9e371a38ed027bcb5b00150f
Ashton <martinezashton03@gmail.com>
2024-12-29 13:29:51 -0500

    Add base path to web hash hitory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,40 +1,40 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import InitScreen from '../views/InitView.vue'
-import LaneView from '../views/LaneView.vue'
-import { useLaneStore } from '../stores/lanes'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import InitScreen from '../views/InitView.vue';
+import LaneView from '../views/LaneView.vue';
+import { useLaneStore } from '../stores/lanes';
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory('/vue-range-app/'), // Add the base path here
   routes: [
     {
       path: '/vue-range-app/',
       name: 'init',
       component: InitScreen,
       beforeEnter: (to, from, next) => {
-        const store = useLaneStore()
-        store.loadFromStorage()
+        const store = useLaneStore();
+        store.loadFromStorage();
         if (store.isInitialized) {
-          next('/vue-range-app/lanes')
+          next('/vue-range-app/lanes');
         } else {
-          next()
+          next();
         }
-      }
+      },
     },
     {
       path: '/vue-range-app/lanes',
       name: 'lanes',
       component: LaneView,
       beforeEnter: (to, from, next) => {
-        const store = useLaneStore()
-        store.loadFromStorage()
+        const store = useLaneStore();
+        store.loadFromStorage();
         if (!store.isInitialized) {
-          next('/vue-range-app/')
+          next('/vue-range-app/');
         } else {
-          next()
+          next();
         }
-      }
-    }
-  ]
-})
+      },
+    },
+  ],
+});
 
-export default router
+export default router;
